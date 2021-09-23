ecf9483c94b6c9f150be74792ad7dfebc8b7b1f6
AAM POWER DEVELOPER <AAM POWER DEVELOPER@DESKTOP-FLAPIQK>
2021-09-23 15:51:29 +0500

    Jetpack Compose Animation

--- app/src/main/java/com/example/comptest/component/CustomComponennt.kt
+++ app/src/main/java/com/example/comptest/component/CustomComponennt.kt
@@ -1,12 +1,17 @@
 package com.example.comptest.component
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -15,20 +20,30 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 @Composable
 fun CustomComponent(
     canvasSize: Dp = 300.dp,
     indicatorValue: Int = 0,
-    maxIndicatorValue: Int = 1000,
-    //backgroundIndicatorColor: Color = MaterialTheme.colors.surface.copy(alpha = 0.1f),
-    backgroundIndicatorColor: Color = Color.Green,
+    maxIndicatorValue: Int = 100,
+    backgroundIndicatorColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f),
+    //indicatorStrokeCap: StrokeCap = StrokeCap.Round,
     backgroundIndicatorStrokeWidth: Float = 100f,
     foregroundIndicatorColor: Color = MaterialTheme.colors.primary,
-    foregroundIndicatorStrokeWidth: Float = 100f
+    foregroundIndicatorStrokeWidth: Float = 100f,
+    bingTextFontSize: TextUnit = MaterialTheme.typography.h3.fontSize,
+    bigTextColor: Color = MaterialTheme.colors.onSurface,
+    binTextSuffix: String = "GB",
+    smallText: String = "Remaining",
+    smallTextFontSize: TextUnit = MaterialTheme.typography.h6.fontSize,
+    smallTextColor: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
 ) {
     var allowedIndicatorValue by remember {
         mutableStateOf(maxIndicatorValue)
@@ -38,36 +53,63 @@ fun CustomComponent(
     } else {
         maxIndicatorValue
     }
-    val animatedIndicatorValue = remember {
-        Animatable(initialValue = 0f)
-    }
+    var animatedIndicatorValue by remember { mutableStateOf(0f) }
     LaunchedEffect(key1 = allowedIndicatorValue) {
-        animatedIndicatorValue.animateTo(allowedIndicatorValue.toFloat())
+        animatedIndicatorValue = allowedIndicatorValue.toFloat()
     }
-    val percentage = (animatedIndicatorValue.value / maxIndicatorValue) * 100
+    val percentage = (animatedIndicatorValue / maxIndicatorValue) * 100
     val sweepAngle by animateFloatAsState(
-        targetValue = (2.4 * percentage).toFloat(),
+        targetValue = (2.4 * percentage).toFloat(), animationSpec = tween(1000)
+    )
+
+    val receivedValue by animateIntAsState(
+        targetValue = allowedIndicatorValue,
         animationSpec = tween(1000)
     )
-    Column(modifier = Modifier
-        .size(canvasSize)
-        .drawBehind {
-            val componentSize = size / 1.25f
-            backgroundIndicator(
-                componentSize = componentSize,
-                indicatorColor = backgroundIndicatorColor,
-                indicatorStrokeWidth = backgroundIndicatorStrokeWidth,
-            )
-            foregroundIndicator(
-                sweepAngle = sweepAngle,
-                componentSize = componentSize,
-                indicatorColor = foregroundIndicatorColor,
-                indicatorStrokeWidth = foregroundIndicatorStrokeWidth
-            )
 
-        }
+    val animateBigTextColor by animateColorAsState(
+        targetValue = if (allowedIndicatorValue == 0) {
+            MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
+        } else if (allowedIndicatorValue >= maxIndicatorValue) {
+            MaterialTheme.colors.error
+        } else
+            bigTextColor, animationSpec = tween(1000)
+    )
+    Column(
+        modifier = Modifier
+            .size(canvasSize)
+            .drawBehind {
+                val componentSize = size / 1.25f
+                backgroundIndicator(
+                    componentSize = componentSize,
+                    indicatorColor = backgroundIndicatorColor,
+                    indicatorStrokeWidth = backgroundIndicatorStrokeWidth,
+                    //indicatorStrokeCap = indicatorStrokeCap
+                )
+                foregroundIndicator(
+                    sweepAngle = sweepAngle,
+                    componentSize = componentSize,
+                    indicatorColor = if (allowedIndicatorValue < maxIndicatorValue) foregroundIndicatorColor else {
+                        Color.Red
+
+                    },
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth
+                    //indicatorStrokeCap = indicatorStrokeCap
+                )
+
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        EmbeddedEmlements(
+            bigText = receivedValue,
+            bigTextFontSize = bingTextFontSize,
+            bingTextColor = animateBigTextColor,
+            bigTextSuffix = binTextSuffix,
+            smallText = smallText,
+            smallTextColor = smallTextColor,
+            smallTextFontSize = smallTextFontSize
+        )
     }
 }
 
@@ -75,7 +117,8 @@ private fun DrawScope.foregroundIndicator(
     sweepAngle: Float,
     componentSize: Size,
     indicatorColor: Color,
-    indicatorStrokeWidth: Float
+    indicatorStrokeWidth: Float,
+    //   indicatorStrokeCap: StrokeCap
 ) {
     drawArc(
         size = componentSize,
@@ -94,10 +137,36 @@ private fun DrawScope.foregroundIndicator(
     )
 }
 
+@Composable
+fun EmbeddedEmlements(
+    bigText: Int,
+    bigTextFontSize: TextUnit,
+    bingTextColor: Color,
+    bigTextSuffix: String,
+    smallText: String,
+    smallTextColor: Color,
+    smallTextFontSize: TextUnit
+) {
+    Text(
+        text = smallText,
+        color = smallTextColor,
+        fontSize = smallTextFontSize,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "$bigText ${bigTextSuffix.take(2)}",
+        color = bingTextColor,
+        fontSize = bigTextFontSize,
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
+}
+
 private fun DrawScope.backgroundIndicator(
     componentSize: Size,
     indicatorColor: Color,
-    indicatorStrokeWidth: Float
+    indicatorStrokeWidth: Float,
+    // indicatorStrokeCap: StrokeCap
 ) {
     drawArc(
         size = componentSize,
